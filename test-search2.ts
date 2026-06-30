320660335c52a74e5e88e56cc994713da312f5a5
piyyu <piyushkr446@gmail.com>
2026-06-30 15:54:53 +0530

    chore: clean up test logs

--- test-search2.ts
+++
@@ -1,7 +0,0 @@
-import { JioSaavnProvider } from "./src/providers/jiosaavn-provider";
-async function run() {
-  const p = new JioSaavnProvider();
-  const songs = await p.searchSong("believer");
-  console.log(JSON.stringify(songs, null, 2));
-}
-run();
